7a8fd4e7e90ce7accdb10396f9356645717650b3
Mounir El yacoubi <mounirelyacoubi94@gmail.com>
2019-07-31 11:08:14 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
  
 const SlackBot = require("slackbots");
+const schedule = require('node-schedule');
+const sleep = require('system-sleep')
+var request = require('request');
 
 const express = require("express");
 const app = express();
 
 const matcher = require("./macher");
 
-//
-const fs = require('fs')
 
+//const fs = require('fs')
+var options;
+var auth;
+ var collaborateurId;
  var orderName;
  var abssanceType;
  var startDate;
  var endDate
- var collaborateurId;
- var sendVer;
+ var collaborateurs;
+ var users;
+ var projects;
+ var tasks;
+ var projectId;
+ var task = {
+                imputations:[],
+                collaboratorId: [],
+                startDate,
+                endDate
+            };
+ var duration;
 
  var ver;
  var notUnderstand;
@@ -22,11 +37,45 @@ const fs = require('fs')
  //chaine de vérification
  var str1
  
-	// creation d'un slack bot
+    // creation d'un slack bot
     var bot = new SlackBot({
         token: "xoxb-687525419360-688876395300-CsiR1tQa3UPDR75oNIDmRwIf",
         name: "test-bot"
     });
+
+    bot.on("start", function(){
+         getCollaborateursUsersProjects();
+     });
+
+    schedule.scheduleJob({hour: 10, minute:(23)}, function(){
+
+        var str2 = "\n"
+        for(var i=1 ; i<=projects.length ; i++)
+           str2 = str2+i+"- "+projects[i-1].name+"\n";
+        bot.postMessage("DKXTDHGUA","Bonjour! sur quel projet vous allez travailler aujourd'hui?"+str2);
+        bot.on("message", function(data) {
+        
+        if (data.type !== "message") {
+            return;
+        }
+        
+        
+        if(data.user){
+
+              if(projectId && task.imputations.length === 0)
+                getTaskId(data);
+              if(projectId && task.imputations.length !=0){
+                /*
+                bot.postMessage(data.channel,"projectId : "+projectId+"  tasks : "+str);*/
+                proiectId = null;
+                task = [];
+              }
+              if(!projectId)
+                getProjectId(data);
+            }
+          })
+
+      })
 
     
     //lencement d'un event message
@@ -35,27 +84,28 @@ const fs = require('fs')
         if (data.type !== "message") {
             return;
         }
-        getCollaborateurId();
-        if(collaborateurId)
-        console.log(collaborateurId);
+        
         if(data.user){
-            getCollaborateurId();
+            getCollaborateurId(collaborateurs, users, data);
             //if(collaborateurId){
                 handleMessage(data.text , data);
             /*}else{
                 bot.postMessage(data.channel, "Pardon! je peux pas vous aider");
             }*/
         }
-        if(orderName && abssanceType && startDate && endDate && !sendVer){
+        if(orderName && abssanceType && startDate && endDate){
             bot.postMessage(data.channel, "order : "+orderName+" ; abssanceType : "+abssanceType+" ; startDate : "
-                                          +startDate+" ; endDate : "+endDate );
-            sendVer = 1;
+                                          +startDate+" ; endDate : "+endDate )
+            orderName = null;
+            abssanceType = null;
+            startDate = null;
+            enddate = null;
         }
     });
 
     //fonction du traitement des message
     function handleMessage(message , data) {
-        // vider la chaine de vérification
+
         matcher(data.text, messageData =>{
 
                 if(!messageData.intent){
@@ -67,8 +117,9 @@ const fs = require('fs')
 
         if(!notUnderstand){
         
+            // vider la chaine de vérification
             str1 ="";
-            str = data.text.split(" ");
+            str = data.text.split(/\s+/);
 
             for (i=0 ; i<str.length ; i++){
                 matcher(str[i], messageData =>{
@@ -76,7 +127,7 @@ const fs = require('fs')
                     case "hello":
                         var hour = new Date().getHours();
                         if(hour>5 && hour<18)
-               	            sendGreetingMorning(data.channel);
+                            sendGreetingMorning(data.channel);
                         else
                             sendGreetingNight(data.channel);
                     break;
@@ -98,7 +149,12 @@ const fs = require('fs')
             }
         
             if(orderName && !abssanceType && !startDate && !endDate){
-                bot.postMessage(data.channel, "de quel type?");
+                bot.postMessage(data.channel, "de quel type?\n"+
+                                              "- congé payé\n"+
+                                              "- congé de naissance\n"+
+                                              "- congé de maternité\n"+
+                                              "- congé de maladie\n"+
+                                              "- congé de mariage\n");
                 for (i=0 ; i<str.length ; i++){
                     getOrderType(str[i],data);
                     getStarAndEndDat(str[i],data);
@@ -134,7 +190,7 @@ const fs = require('fs')
                 endDate = null;
             }
 
-            if((!endDate || !startDate) && (endDate || startDate) ){
+            if((!orderName || !abssanceType) && (endDate || startDate) ){
                 bot.postMessage(data.channel, "pardon, vous devez d'abord spécifier votre demande");
                 startDate=null;
                 enddat=null;
@@ -156,7 +212,7 @@ const fs = require('fs')
         bot.postMessage(channel, greeting);
     }
 
- 	//fonctions pour générer des messages aléatoire pour dire hello
+    //fonctions pour générer des messages aléatoire pour dire hello
     function getGreetingMorning() {
         var greetings = [
             "Bonjour! comment je peux vous aider",
@@ -197,44 +253,49 @@ const fs = require('fs')
         matcher(str, messageData =>{
         switch(messageData.intent){
             case "payé":
-                abssanceType = 31;
                 if(!orderName){
                     bot.postMessage(data.channel, "Pardon, j'ai pas conpris.\n"
-                                                   +"Je ponse que vous voulez un congé payé ");
+                                                  +"Je ponse que vous voulez un congé payé ");
                 }
+                else
+                     abssanceType = 31;
                 
             break;
 
             case "naissance":
-                abssanceType = 64;
                 if(!orderName){
                     bot.postMessage(data.channel, "Pardon, j'ai pas conpris.\n"
                                                    +"Je ponse que vous voulez un congé de naissance");
                 }
+                else
+                     abssanceType = 64;
             break;
 
             case "maternité":
-                abssanceType = 10;
                 if(!orderName){
                     bot.postMessage(data.channel, "Pardon, j'ai pas conpris.\n"
                                                    +"Je ponse que vous voulez un congé maternité");
                 }
+                else
+                     abssanceType = 10;
             break;
 
             case "maladie":
-                abssanceType = 7;
                 if(!orderName){
                     bot.postMessage(data.channel, "Pardon, j'ai pas conpris.\n"
                                                    +"Je ponse que vous voulez un congé maladie");
                 }
+                else
+                    abssanceType = 7;
             break;
 
             case "mariage":
-                abssanceType = 6;
                 if(!orderName){
                     bot.postMessage(data.channel, "Pardon, j'ai pas conpris.\n"
                                                    +"Je ponse que vous voulez un congé mariage");
                 }
+                else
+                    abssanceType = 6;
             break;
 
             default :
@@ -248,7 +309,7 @@ const fs = require('fs')
        matcher(str, messageData =>{
         switch(messageData.intent){
                 case "date":
-                    if(str1 ==="du"){
+                    if(str1 ==="du" || str1 ==="Du" || str1 ==="DU"){
                         if(isNaN(new Date(str)) || new Date(str)<=new Date()){
                             bot.postMessage(data.channel, "la date de début de congé invalide ("+str+")");
                         }
@@ -278,28 +339,20 @@ const fs = require('fs')
     //fonction pour créer et envoier le json
     function creatJsonFile(){
 
-    	let order = {
-   			"order" : orderName,
-   			"type" : orderType,
-		}
- 
-		/*app.get("/url", function(req, res, next) {
- 			res.json(order);
-		});
-		app.listen(3000, function(){
- 			console.log("Server running on port 3000");
-		});*/   
+        let order = {
+            "order" : orderName,
+            "type" : orderType,
+        }
     }
 
 
-    function getCollaborateurId(){
-        var request = require('request')
+    function getCollaborateursUsersProjects(){
         var username = 'jenun@intempmail.com'
         var password = 'N0velis'
         var obj;
         var userEmail;
 
-        var options = {
+        options = {
             url: 'http://recette-novy-api.novelis.io/api/authenticate',
             body: JSON.stringify({
                 username: username,
@@ -322,9 +375,9 @@ const fs = require('fs')
 // la deuxième requete 
 
         options = {
-            url: 'https://slack.com/api/users.profile.get?token=xoxp-687525419360-676190667394'+
+            url: 'https://slack.com/api/users.list?token=xoxp-687525419360-676190667394'+
                  '-698919274613-3b5b02d68e29f20e6b51670ccf8e2716'+
-                 '&user=UKW5LKMBL&pretty=1',
+                 '&pretty=1',
   
             method : 'get'
         }
@@ -334,10 +387,12 @@ const fs = require('fs')
             console.log(err)
             return
             }
-            userEmail = JSON.parse(body).profile.email;
+             
+            users = JSON.parse(body).members;
+            
         })
-/////////////////////////////////
-        var auth = "Bearer "+ strAuth;
+///////////////////////////
+        auth = "Bearer "+ strAuth;
         options = {
             url: 'http://recette-novy-api.novelis.io/collaborator/api/collaborators?activated=true',
             headers : { 
@@ -351,14 +406,158 @@ const fs = require('fs')
                 console.log(err)
                 return
             }
-        for(i=0 ; i<JSON.parse(body).length ; i++){
-            if(userEmail === JSON.parse(body)[i].email)
-            collaborateurId = JSON.parse(body)[i].id;
-            return;
+        
+           collaborateurs = JSON.parse(body);
+    })
+
+    options = {
+        url: 'http://recette-novy-api.novelis.io/collaborator/api/projects?activated=true',
+        headers : { 
+            'Authorization' : auth,
+            'Content-Type' : 'application/json'
+        },
+        method : 'get'
+    }
+
+    request(options, function (err, res, body) {
+        if (err) {
+            console.log(err)
+            return
         }
-        collaborateurId = null;
+  
+    projects = JSON.parse(body);
+  
     })
 
     })
-        
 }
+
+    function getCollaborateurId(collaborateurs, users, data){
+
+        var UserEmail
+        var i =0;
+
+        while(users[i].id != data.user && i < users.length-1){
+            i++;
+        }
+        userEmail = users[i].profile.email;
+
+        for(i=0 ; i<collaborateurs.length ; i++){
+            if(userEmail === collaborateurs[i].email)
+            collaborateurId = collaborateurs[i].id;
+            return;
+        }
+
+    }
+
+    //////
+    function getProjectId(data){
+      var str = data.text.split(/\s+/);
+              for (i=0 ; i<str.length ; i++){
+                matcher(str[i], messageData =>{
+                switch(messageData.intent){
+                        case "projectNumber":
+                          var num = parseInt(str, 10)
+                          if(num > projects.length){
+                            bot.postMessage(data.channel,"pardon, vous devez choisir le numèro du projet");
+                            break;
+                          }
+                          bot.postMessage(data.channel,"vous avez choisir le projet : "+projects[num-1].name);
+                          projectId = projects[num-1].id;
+                          tasks = projects[num-1].tasks
+                          var str1 = "\n";
+                          for(var j=1 ; j<=tasks.length ; j++){
+                            str1 = str1+j+"- "+tasks[j-1].description+"\n";
+                          }
+                          bot.postMessage("DKXTDHGUA","définir la tache avec la durée estimé pour chaqu'une "+str1);
+                        break;
+                    default :
+                    break;
+                    }
+                  })
+                }
+        }
+
+        //
+        function getTaskId(data){
+            var request = require('request');
+
+            var date = new Date();
+            var startDate1 = new Date(date.getDate() - date.getDay()+1);
+            var endDate1 = new Date(date.getDate() - date.getDay()+7);
+            
+            var str = data.text.split(/\n/);
+                console.log(str[0]);
+              for (i=0 ; i<str.length ; i++){
+                var str1 = str[i].split(/\s+/);
+                matcher(str1[0], messageData =>{
+                switch(messageData.intent){
+                        case "projectNumber":
+                        
+                          var num = parseInt(str1[0], 10)
+                          if(num > tasks.length){
+                            bot.postMessage(data.channel,"pardon, vous devez choisir le numèro de la tache");
+                            break;
+                          }
+
+                           var imputationDate = date.getDate()+'-'+(date.getMonth()+1)+'-'+date.getFullYear();
+
+                           task.imputations.push({"id" : i , "date" : imputationDate  , "duration" : str1[1] , "taskId" : tasks[num-1].id , "ressource" : 71});
+                        break;
+                    default :
+                    break;
+                    }
+                  })
+                }
+                task.collaboratorId.push(71);
+
+                var strStartDate = startDate1.getDate()+'-'+(startDate1.getMonth()+1)+'-'+startDate1.getFullYear();
+                var strEndDate = endDate1.getDate()+'-'+(endDate1.getMonth()+1)+'-'+endDate1.getFullYear();
+                task.startDate = strStartDate;
+                task.endDate = strEndDate;
+                
+                options = {
+                    url: 'http://recette-novy-api.novelis.io/collaborator/api/imputations/save',
+                    headers : { 
+                        'Authorization' : auth,
+                        'Content-Type' : 'application/json'
+                    },
+                    body: task,
+                    json:true,
+                    method : 'post'
+                }
+
+    request(options, function (err, res, body) {
+        if (err) {
+            console.log(err)
+            return
+        }
+        console.log(body);
+  
+    });
+
+    console.log(task);
+ }
+
+        /*//
+        function getDuration(data){
+          var str = data.text.split(/\s+/);
+              for (i=0 ; i<str.length ; i++){
+                matcher(str[i], messageData =>{
+                  console.log("hi")
+                switch(messageData.intent){
+                        case "projectNumber":
+                          var num = parseInt(str, 10)
+                          if(num > 12){
+                            bot.postMessage(data.channel,"pardon, vous devez choisir le numèro de la durée");
+                            break;
+                          }
+                          console.log(num);
+                          duration = num/8;
+                        break;
+                    default :
+                    break;
+                    }
+                  })
+                }
+        }*/
